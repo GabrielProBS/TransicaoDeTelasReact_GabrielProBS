eb58d8afd1c3b16a30c96417ec436247b8e30665
GabrielAlunoDS <gabriel.silva3804@etec.sp.gov.br>
2025-05-21 08:53:57 -0300

    Update DetailsScreen.js

--- MeuAppDeNavegacao/src/screens/DetailsScreen.js
+++ MeuAppDeNavegacao/src/screens/DetailsScreen.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet, Dimensions} from 'react-native';
 
-const windowWidth = Dimensions.get('window').width;
+const windowWidth= Dimensions.get('window').width;
 
-export default function HomeScreen ({navigation}){
-    return (
+export default function DetailsScreen({navigation}){
+    return(
         <View style={styles.container}>
-            <Text style={styles.title}>Home Screen</Text>
+            <Text style= {styles.title}> Details </Text>
             <View style={styles.buttonContainer}>
                 <Button
-                    title="Go to details"
-                    onPress={() => navigation.navigate('Details')}
+                title= "Go to home"
+                onPress={() => navigation.navigate('Home')}
                 />
             </View>
-        <View style={styles.buttonContainer}>
-            <Button
+            <View style={styles.buttonContainer}>
+                <Button
                 title="Go to profile"
                 onPress={() => navigation.navigate('Profile')}
-            />
+                />
+            </View>
         </View>
-    </View>
-    )
+    );
 };
 
-const styles= StyleSheet.create({
+const styles=StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        flex:1,
+        justifyContent:'center',    
         alignItems: 'center',
-        backgroundColor: '#8A8C46', //cor de fundo da tela do app
+        backgroundColor: '#8A8C46',
     },
     title: {
-        fontSize: 24,
+        fontSize:24,
         marginBottom: 20,
-        fontFamily: 'monospace',
+        fontfamily: 'monospace',
     },
     buttonContainer: {
         backgroundColor: '#403D1C',
-        margin: 10,
+        margin:10,
         width: windowWidth *0.5,
         borderRadius: 5,
     },
